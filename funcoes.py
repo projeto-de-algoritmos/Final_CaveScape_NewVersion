f3481c0a59ac4eb4290cd4327233b0e6c9dbd23e
Ugor Marcilio Brandão Costa <52638444+ubrando@users.noreply.github.com>
2022-04-21 14:51:57 -0300

    prim problem implementation

---
+++ funcoes.py
@@ -0,0 +1,136 @@
+import random
+import sys
+
+# -- KanapSack ---
+
+
+def knapSack(W, wt, val, n):
+    K = [[0 for x in range(W + 1)] for x in range(n + 1)]
+    for i in range(n + 1):
+        for w in range(W + 1):
+            if i == 0 or w == 0:
+                K[i][w] = 0
+            elif wt[i-1] <= w:
+                K[i][w] = max(val[i-1]
+                              + K[i-1][w-wt[i-1]],
+                              K[i-1][w])
+            else:
+                K[i][w] = K[i-1][w]
+
+    return K[n][W]
+
+
+def mudarValoresKnapSack(W, wt, val, n):
+    wt.clear()
+    val.clear()
+    for i in range(0, 5):
+        wt.append(random.randint(1, 32))
+        val.append(random.randint(1, 32))
+    wt.sort()
+    val.sort()
+    W = random.randint(15, 30)
+    n = len(val)
+    return W, wt, val, n
+
+
+# --- Grafos 2 - PRIM ---
+vetorPontosTuristicos = ["Praia Leblon", "Rocinha", "Lagoa", "Jardim Botânico", " Cristo Redentor",
+                         "Sambódramo", "Aeroporto", "Praia de Copacabana", "Pão De Açúcar", "Baía de Guanabara"
+                         ]
+
+
+class Graphico():
+
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)]
+                      for row in range(vertices)]
+
+    def printMST(self, parent):
+        totalMap = 0
+        new_line = "\n"
+        resultMap = ""
+        for i in range(1, self.V):
+            resultMap += '{} -  {}  R$ {},00 . '.format(
+                vetorPontosTuristicos[parent[i]], vetorPontosTuristicos[i], self.graph[i][parent[i]], new_line)
+            resultMap += "\n"
+            totalMap += self.graph[i][parent[i]]
+
+        return resultMap
+
+    def printMST1(self, parent):
+        totalMap = 0
+        for i in range(1, self.V):
+            totalMap += self.graph[i][parent[i]]
+
+        return totalMap
+
+    def minKey(self, key, mstSet):
+
+        min = sys.maxsize
+
+        for v in range(self.V):
+            if key[v] < min and mstSet[v] == False:
+                min = key[v]
+                min_index = v
+
+        return min_index
+
+    def primMST(self):
+
+        key = [sys.maxsize] * self.V
+        parent = [None] * self.V
+        key[0] = 0
+        mstSet = [False] * self.V
+
+        parent[0] = -1
+
+        for cout in range(self.V):
+            u = self.minKey(key, mstSet)
+            mstSet[u] = True
+            for v in range(self.V):
+                if self.graph[u][v] > 0 and mstSet[v] == False and key[v] > self.graph[u][v]:
+                    key[v] = self.graph[u][v]
+                    parent[v] = u
+        return(self.printMST(parent))
+
+    def primMST1(self):
+
+        key = [sys.maxsize] * self.V
+        parent = [None] * self.V
+        key[0] = 0
+        mstSet = [False] * self.V
+
+        parent[0] = -1
+
+        for cout in range(self.V):
+            u = self.minKey(key, mstSet)
+            mstSet[u] = True
+            for v in range(self.V):
+                if self.graph[u][v] > 0 and mstSet[v] == False and key[v] > self.graph[u][v]:
+                    key[v] = self.graph[u][v]
+                    parent[v] = u
+        return(self.printMST1(parent))
+
+
+def mudarValoresArestas():
+    aresta1 = random.randint(1, 52)
+    aresta2 = random.randint(1, 52)
+    aresta3 = random.randint(1, 52)
+    aresta4 = random.randint(1, 52)
+    aresta5 = random.randint(1, 52)
+    aresta6 = random.randint(1, 52)
+    aresta7 = random.randint(1, 52)
+    aresta8 = random.randint(1, 52)
+    aresta9 = random.randint(1, 52)
+    aresta10 = random.randint(1, 52)
+    aresta11 = random.randint(1, 52)
+    aresta12 = random.randint(1, 52)
+    aresta13 = random.randint(1, 52)
+    aresta14 = random.randint(1, 52)
+    aresta15 = random.randint(1, 52)
+    aresta16 = random.randint(1, 52)
+    aresta17 = random.randint(1, 52)
+    aresta18 = random.randint(1, 52)
+    aresta19 = random.randint(1, 52)
+    return aresta1, aresta2, aresta3, aresta4, aresta5, aresta6, aresta7, aresta8, aresta9, aresta10, aresta11, aresta12, aresta13, aresta14, aresta15, aresta16, aresta17, aresta18, aresta19
--- main.py
+++ main.py
@@ -1,38 +1,11 @@
+from funcoes import knapSack, mudarValoresKnapSack, Graphico, vetorPontosTuristicos, mudarValoresArestas
 from cgitb import html
 from webbrowser import get
 from flask import Flask, render_template, request, flash, redirect, url_for
 import random
 
-# -- Estruturas e Funcoes ---
 
-
-def knapSack(W, wt, val, n):
-    K = [[0 for x in range(W + 1)] for x in range(n + 1)]
-    for i in range(n + 1):
-        for w in range(W + 1):
-            if i == 0 or w == 0:
-                K[i][w] = 0
-            elif wt[i-1] <= w:
-                K[i][w] = max(val[i-1]
-                              + K[i-1][w-wt[i-1]],
-                              K[i-1][w])
-            else:
-                K[i][w] = K[i-1][w]
-
-    return K[n][W]
-
-
-def mudarValoresKnapSack(W, wt, val, n):
-    wt.clear()
-    val.clear()
-    for i in range(0, 5):
-        wt.append(random.randint(1, 32))
-        val.append(random.randint(1, 32))
-    wt.sort()
-    val.sort()
-    W = random.randint(15, 30)
-    n = len(val)
-    return W, wt, val, n
+# -- Estruturas---
 
 
 adj_list = {
@@ -57,6 +30,44 @@ global W
 W = 11
 global n
 n = len(val)
+global aresta1
+global aresta2
+global aresta3
+global aresta4
+global aresta5
+global aresta6
+global aresta7
+global aresta8
+global aresta9
+global aresta10
+global aresta11
+global aresta12
+global aresta13
+global aresta14
+global aresta15
+global aresta16
+global aresta17
+global aresta18
+global aresta19
+aresta1 = 37
+aresta2 = 20
+aresta3 = 12
+aresta4 = 13
+aresta5 = 9
+aresta6 = 40
+aresta7 = 36
+aresta8 = 2
+aresta9 = 51
+aresta10 = 4
+aresta11 = 28
+aresta12 = 29
+aresta13 = 45
+aresta14 = 52
+aresta15 = 27
+aresta16 = 6
+aresta17 = 18
+aresta18 = 25
+aresta19 = 33
 
 # --- Aplicacao ---
 
@@ -82,13 +93,13 @@ def homepage():
 
         if (palpite in adj_list[no]):
             no = palpite
-            return redirect(url_for('caminho', no_destino=no))
+            return redirect(url_for('knapsack', no_destino=no))
         else:
             return redirect(url_for('errou'))
 
 
-@app.route('/caminho/<no_destino>', methods=['GET', 'POST'])
-def caminho(no_destino):
+@app.route('/knapsack/<no_destino>', methods=['GET', 'POST'])
+def knapsack(no_destino):
     no = no_destino
     global val
     global wt
@@ -96,7 +107,7 @@ def caminho(no_destino):
     global n
     resposta = knapSack(W, wt, val, n)
     if request.method == "GET":
-        return render_template("caminho.html", no=no,
+        return render_template("knapsack.html", no=no,
                                valor0=val[0], peso0=wt[0],
                                valor1=val[1], peso1=wt[1],
                                valor2=val[2], peso2=wt[2],
@@ -110,10 +121,10 @@ def caminho(no_destino):
             valor = int(request.form.get("valor"))
             if(valor == resposta):
                 flash("Resposta Certa!", "success")
-                return redirect(url_for('caminho', no_destino=no))
+                return redirect(url_for('knapsack', no_destino=no))
             if(valor != resposta):
                 flash("Resposta errada! Tente de Novo!", "warning")
-                return redirect(url_for('caminho', no_destino=no))
+                return redirect(url_for('knapsack', no_destino=no))
         except:
             palpite = str(request.form.get("name"))
             if (palpite == "10" and palpite in adj_list[no]):
@@ -121,7 +132,87 @@ def caminho(no_destino):
             if (palpite in adj_list[no]):
                 no = palpite
                 W, wt, val, n = mudarValoresKnapSack(W, wt, val, n)
-                return redirect(url_for('caminho', no_destino=no))
+                return redirect(url_for('knapsack', no_destino=no))
+            else:
+                return redirect(url_for('errou'))
+
+
+@app.route('/prim/<no_destino>', methods=['GET', 'POST'])
+def prim(no_destino):
+    no = no_destino
+    global aresta1
+    global aresta2
+    global aresta3
+    global aresta4
+    global aresta5
+    global aresta6
+    global aresta7
+    global aresta8
+    global aresta9
+    global aresta10
+    global aresta11
+    global aresta12
+    global aresta13
+    global aresta14
+    global aresta15
+    global aresta16
+    global aresta17
+    global aresta18
+    global aresta19
+    g = Graphico(10)
+    g.graph = [[0, aresta1, aresta2, 0, 0, 0, 0, aresta3, 0, 0],
+               [aresta1, 0, aresta4, aresta5, 0, 0, 0, 0, 0, 0],
+               [aresta2, aresta4, 0, aresta14, 0, 0, 0, aresta19, 0, 0],
+               [0, aresta5, aresta6, 0, aresta7, aresta8, 0, 0, 0, 0],
+               [0, 0, aresta14, aresta7, 0, aresta9,
+                   aresta10, aresta12, aresta13, 0],
+               [0, 0, 0, aresta8, aresta9, 0, aresta11, 0, 0, 0],
+               [0, 0, 0, 0, aresta10, aresta11, 0, aresta15, aresta16, 0],
+               [aresta3, 0, aresta19, 0, aresta15,
+                   0, aresta17, 0, aresta16, 0],
+               [0, 0, 0, 0, aresta13, 0, aresta16, aresta17, 0, aresta18],
+               [0, 0, 0, 0, 0, 0, 0, 0, aresta18, 0], ]
+    resultado = g.primMST()
+    total = g.primMST1()
+    if request.method == "GET":
+        return render_template("prim.html", no=no, aresta1=aresta1,
+                               aresta2=aresta2,
+                               aresta3=aresta3,
+                               aresta4=aresta4,
+                               aresta5=aresta5,
+                               aresta6=aresta6,
+                               aresta7=aresta7,
+                               aresta8=aresta8,
+                               aresta9=aresta9,
+                               aresta10=aresta10,
+                               aresta11=aresta11,
+                               aresta12=aresta12,
+                               aresta13=aresta13,
+                               aresta14=aresta14,
+                               aresta15=aresta15,
+                               aresta16=aresta16,
+                               aresta17=aresta17,
+                               aresta18=aresta18,
+                               aresta19=aresta19,
+                               resultado=resultado,
+                               total=total)
+    else:
+        try:
+            valor = int(request.form.get("valor"))
+            if(valor == total):
+                flash("Resposta Certa!", "success")
+                return redirect(url_for('prim', no_destino=no))
+            if(valor != total):
+                flash("Resposta errada! Tente de Novo!", "warning")
+                return f"{valor} {total}"
+        except:
+            palpite = str(request.form.get("name"))
+            if (palpite == "10" and palpite in adj_list[no]):
+                return redirect(url_for('vitoria'))
+            if (palpite in adj_list[no]):
+                no = palpite
+                aresta1, aresta2, aresta3, aresta4, aresta5, aresta6, aresta7, aresta8, aresta9, aresta10, aresta11, aresta12, aresta13, aresta14, aresta15, aresta16, aresta17, aresta18, aresta19 = mudarValoresArestas()
+                return redirect(url_for('prim', no_destino=no))
             else:
                 return redirect(url_for('errou'))
 
@@ -134,11 +225,6 @@ def errou():
 @app.route('/vitoria', methods=['GET', 'POST'])
 def vitoria():
     return render_template("vitoria.html")
-
-
-@app.route('/modal')
-def modal():
-    return render_template('modal.html',)
 
 
 # colocar o site no ar
