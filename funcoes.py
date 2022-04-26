742d30fe4977c9047f652aad3025a4e5ab0ed671
Ugor Marcilio Brandão Costa <52638444+ubrando@users.noreply.github.com>
2022-04-25 23:27:17 -0300

    video

--- funcoes.py
+++ funcoes.py
@@ -134,3 +134,36 @@ def mudarValoresArestas():
     aresta18 = random.randint(1, 52)
     aresta19 = random.randint(1, 52)
     return aresta1, aresta2, aresta3, aresta4, aresta5, aresta6, aresta7, aresta8, aresta9, aresta10, aresta11, aresta12, aresta13, aresta14, aresta15, aresta16, aresta17, aresta18, aresta19
+
+
+# --- GREED ----
+def minimize_lateness(ttimes, dtimes):
+    """Return minimum max lateness and the schedule to obtain it.
+
+    (min_lateness, schedule) is returned.
+
+    Lateness of a request i is L(i) = finish time of i - deadline of if
+    request i finishes after its deadline.
+    The maximum lateness is the maximum value of L(i) over all i.
+    min_lateness is the minimum value of the maximum lateness that can be
+    achieved by optimally scheduling the requests.
+
+    schedule is a list that contains the indexes of the requests ordered such
+    that minimum maximum lateness is achieved.
+
+    ttime[i] is the time taken to complete request i.
+    dtime[i] is the deadline of request i.
+    """
+    # index = [0, 1, 2, ..., n - 1] for n requests
+    index = list(range(len(dtimes)))
+    # sort according to deadlines
+    index.sort(key=lambda i: dtimes[i])
+
+    min_lateness = 0
+    start_time = 0
+    for i in index:
+        min_lateness = max(min_lateness,
+                           (ttimes[i] + start_time) - dtimes[i])
+        start_time += ttimes[i]
+
+    return min_lateness, index
